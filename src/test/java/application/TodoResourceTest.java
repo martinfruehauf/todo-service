61c9a464b4d40ad749fd05d9ff7422f787bd5f37
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-04-22 16:33:47 +0200

    - add Glassfish Dependency to pom.xml - add NameTest to test the constructor for class Name - overwrite toString() and equals() of Name.java - introduce TodoResourceTest.java which is still faulty !!

---
+++ src/test/java/application/TodoResourceTest.java
@@ -0,0 +1,31 @@
+package application;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.ws.rs.core.Response;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TodoResourceTest {
+
+  private TodoResource todoResource;
+
+  @BeforeEach
+  public void setUp() {
+    this.todoResource = new TodoResource();
+  }
+
+  @Test
+    public void testGetName(){
+        JsonArray jsonResponse = Json.createArrayBuilder()
+                .add(Json.createObjectBuilder().add("name", "Max"))
+                .add(Json.createObjectBuilder().add("name", "Christian"))
+                .add(Json.createObjectBuilder().add("name", "Martin")).build();
+        System.out.println(jsonResponse);
+      Response names = this.todoResource.getNames();
+      assertEquals(jsonResponse, names.getEntity());
+  }
+}
