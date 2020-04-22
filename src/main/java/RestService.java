b9c35f478d85116ee732473a2dbf6de7e682c706
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-04-22 09:58:53 +0200

    Change the name of entry calls to RestApplication and strip it of unnecessary code. Also create a RestService class where the actual function is implemented

--- src/main/java/RestService.java
+++
@@ -1,7 +0,0 @@
-import javax.ws.rs.Path;
-
-//The Java Class will be hosted at the URI path "/api"
-@Path ("/api")
-public class RestService {
-
-}
---
+++ src/main/java/TodoResource.java
@@ -0,0 +1,12 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path ("/todos")
+public class TodoResource {
+
+    // The java method will produce HTTP GET requests
+    @GET
+
+
+    // The java method will
+}
