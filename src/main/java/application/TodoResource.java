4d5d2c94373772c9594ba5868c6fc26a905161b3
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-04-28 17:33:46 +0200

    still fail to call to-do-methods by id

--- src/main/java/application/TodoResource.java
+++ src/main/java/application/TodoResource.java
@@ -1,15 +1,17 @@
 package application;
 
-import domain.Todo;
-import domain.TodoService;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
+
+import domain.Todo;
+import domain.TodoService;
 
 @Path("/todos")
 @Produces(MediaType.APPLICATION_JSON)
@@ -17,17 +19,26 @@ public class TodoResource {
   @Inject
   private TodoService todoService;
 
+
   public TodoResource() {
   }
 
   public TodoResource(final TodoService todoService) {
     this.todoService = todoService;
+    List<Todo> list = todoService.listTodo();
   }
 
   @GET
   public Response getTodos() {
-    List<Todo> list = todoService.listTodo();
+    //List<Todo> list = todoService.listTodo();
     return Response.ok().entity(list).build();
+  }
+
+  @GET
+  @Path("/{todoId}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getTodoById(@PathParam("todoId") long todoId){
+    return Response.ok().entity(list.get(todoId)).build();
   }
 
 }
