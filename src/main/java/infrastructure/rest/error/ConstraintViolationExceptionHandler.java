ee3577fced58ba13631cd1d912c9e9383e0c383f
martinfruehauf <martin.fruehauf@openknowledge.de>
2020-05-20 10:15:18 +0200

    Add constraint violation exception handling

---
+++ src/main/java/infrastructure/rest/error/ConstraintViolationExceptionHandler.java
@@ -0,0 +1,29 @@
+package infrastructure.rest.error;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+    private static final Logger LOG = LoggerFactory.getLogger(ConstraintViolationExceptionMapper.class);
+
+    @Override
+    public Response toResponse(ConstraintViolationException e) {
+        LOG.warn("Request validation failed");
+        List<ValidationErrorDTO> errors = e.getConstraintViolations().stream()
+                .map(ValidationErrorDTO::new)
+                .collect(Collectors.toList());
+
+        return Response.status(Response.Status.BAD_REQUEST).entity(errors).build();
+    }
+}
+
